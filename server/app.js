4dd613af14b8295c0a2416de62bef6ffbbfcd8be
Rahul Panchal <rm5901960@gmail.com>
2023-12-26 13:22:00 +0530

    Fixed callback url

--- server/app.js
+++ server/app.js
@@ -18,7 +18,7 @@ const config = {
 }
 
 const AUTH_OPTIONS = {
-    callbackURL: '/auth/google/callback', 
+    callbackURL: 'https://chatroom-gy71.onrender.com/auth/google/callback', 
     clientID: config.CLIENT_ID,
     clientSecret: config.CLIENT_SECRET
 }
