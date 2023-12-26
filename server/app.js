a9759e674c1a52ab6e5a4324ed7b91a060019071
Rahul Panchal <rm5901960@gmail.com>
2023-12-26 12:33:39 +0530

    Adding the google login using  oAuth2.0

--- server/app.js
+++ server/app.js
@@ -1,13 +1,106 @@
+require('dotenv').config();
 const express = require('express');
+
+const path = require('path');
+const passport = require('passport');
+const {Strategy} = require('passport-google-oauth20');
+const cookieSession = require('cookie-session')
+const {verify}=require('crypto')
+
+const helmet = require('helmet')
+
+const config = {
+    CLIENT_ID: process.env.CLIENT_ID,
+    CLIENT_SECRET: process.env.CLIENT_SECRET,
+    COOKIE_KEY_1:process.env.COOKIE_KEY_1,
+    COOKIE_KEY_2:process.env.COOKIE_KEY_2
+    
+}
+
+const AUTH_OPTIONS = {
+    callbackURL: '/auth/google/callback', 
+    clientID: config.CLIENT_ID,
+    clientSecret: config.CLIENT_SECRET
+}
+
+function verifyCallback(accessToken, refreshToken,profile,done){
+    console.log('Google Profile',profile._json,"\nProvided by ",profile.provider);
+    done(null,profile);
+}
+
+passport.use(new Strategy(AUTH_OPTIONS,verifyCallback))
+//saving the session to cookie
+passport.serializeUser((user,done)=>{
+    done(null,user._json);
+})
+//reading session from cookie
+passport.deserializeUser((obj,done)=>{
+    done(null,obj);
+})
+
 const app = express();
+app.use(helmet());
+app.use(cookieSession({
+    name:'session',
+    maxAge:24*60*60*1000,
+    keys: [config.COOKIE_KEY_1, config.COOKIE_KEY_2]
+}))
+app.use(passport.initialize())
+app.use(passport.session())
+
+function checkLoggedIn(req,res,next){
+    
+    const isLoggedIn = req.isAuthenticated();
+    //console.log(profile);
+    if(!isLoggedIn){
+        return res.status(401).json({
+            error:'You must log in!'
+        })
+    }
+    console.log('Current user is: ',req.user.name)
+    next();
+}
 
 
-// app.use(express.static(
-//     path.join(__dirname,'..','client')
-// ));
+app.get('/auth/google',
+    passport.authenticate('google',{
+        scope: ['email','profile']
+    }),
+    (req,res)=>{
+        console.log("Data came!.....")
+    }
+);
+
+app.get('/auth/google/callback', 
+    passport.authenticate('google' , {
+        failureRedirect: '/failure',
+        successRedirect:'/chat',
+        session: true,
+    }),
+    (req,res)=>{
+        console.log("Google called us back")
+    }
+);
+app.get('/auth/logout',(req,res)=>{
+    console.log('logging out');
+    req.logOut()
+})
+
+app.get('/failure',(req,res)=>{
+    res.send("Something Went Wrong")
+})
 
 app.get('/',(req,res)=>{
-    res.send('api is live')
+    res.sendFile(path.join(__dirname,"..","client","landing_page.html"))
+})
+
+app.use(express.static(
+    path.join(__dirname,'..','client')
+));
+
+app.get('/chat',checkLoggedIn,(req,res)=>{
+    console.log('Serving Home Page');
+    res.sendFile(path.join(__dirname,"..","client","index.html"));
 })
 
 
--- server/server.js
+++ server/server.js
@@ -11,9 +11,10 @@ io.on('connection',(socket)=>{
     console.log(`A user Connected | ${socket.id}`);
     socket.on('message',(data)=>{
         console.log(data)
-        console.log(data.msg,"<<<<<",data.username)
-        socket.broadcast.emit('message',data);
+        
+        //socket.broadcast.emit('message',data);
         console.log(data.username,">>>>>",data.msg)
+        socket.to(room_id).emit('message',"Message to te rooom",room_id)
     });
     socket.on('newuser',(data)=>{
         console.log(data)
@@ -22,7 +23,15 @@ io.on('connection',(socket)=>{
     socket.on('disconnect',(data)=>{
         console.log(data)
         socket.broadcast.emit('userLeft',data)
-    })
+    });
+
+
+    //private room
+    const room_id="a"
+    socket.join(room_id,()=>{
+        console.log('joined a room')
+    });
+    
 
 
 })
